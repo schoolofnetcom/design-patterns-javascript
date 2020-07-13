a84125a669fce6c439321a74150aaca16ae2dbe8
valls <olavalls@gmail.com>
2020-07-12 21:54:29 -0300

    Creating first singleton example

--- singleton/index.js
+++ singleton/index.js
@@ -1,1 +1,20 @@
-console.log('singleton pattern');
+class Singleton {
+  static getInstance() {
+    if (!this.instance) {
+        this.instance = new Object;
+        this.instance.name = "new object";
+        this.instance.version = Math.floor(Math.random() * 1000);
+    } 
+    return this.instance;
+  };
+}
+
+
+function compare(object1, object2) {
+  return object1 === object2;
+}
+
+const object1 = Singleton.getInstance();
+const object2 = Singleton.getInstance();
+
+console.log(compare(object1, object2));
